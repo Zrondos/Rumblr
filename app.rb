f76197bfb721871af67efbc7aa75b51d08308426
zrondos <zrondos@github.com>
2018-09-18 18:53:45 -0400

    Sign_in, sign_up, and logout pages work

--- app.rb
+++ app.rb
@@ -19,12 +19,21 @@ post '/sign_up' do
     session[:user_id]=user.id
 end
 
-get 'sign_in' do
+get '/sign_in' do
     erb :sign_in
 end
 
-post 'sign_in' do 
-    user=User.create(
-        username: params[:username],
-        password: params[:password]
-    )
+post '/sign_in' do 
+    user = User.find_by(username: params[:username])
+    if user && user.password == params[:password]
+        session[:user_id] = user.id
+        flash[:info] = 'You have been signed in'
+    else
+        flash[:error] = 'Incorrect username and/or password'
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect '/'
+end
